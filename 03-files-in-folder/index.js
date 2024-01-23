c172f14120bddd42e56a1f27ae1ae5f566a410d5
veta306 <vv097099@gmail.com>
2024-01-23 17:34:12 +0200

    feat: complete 03-files-in-folder using async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,21 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const dirPath = path.join(__dirname, 'secret-folder');
-fs.readdir(dirPath, (error, files) => {
-  if (error) return console.log(error.message);
-  files.forEach((filename) => {
+async function showFiles() {
+  const files = await fs.readdir(dirPath);
+  for (const filename of files) {
     const filePath = path.join(dirPath, filename);
-    fs.stat(filePath, (error, stats) => {
-      if (error) return console.log(error.message);
-      if (stats.isFile()) {
-        console.log(
-          path.basename(filePath, path.extname(filePath)) +
-            ' - ' +
-            path.extname(filePath).split('.')[1] +
-            ' - ' +
-            (stats.size / 1024).toFixed(2) +
-            'kb',
-        );
-      }
-    });
-  });
-});
+    const stats = await fs.stat(filePath);
+    if (stats.isFile())
+      console.log(
+        path.basename(filePath, path.extname(filePath)) +
+          ' - ' +
+          path.extname(filePath).split('.')[1] +
+          ' - ' +
+          (stats.size / 1024).toFixed(2) +
+          'kb',
+      );
+  }
+}
+showFiles();
