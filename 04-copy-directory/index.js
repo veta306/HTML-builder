c6d96ab05f3319d7f0bac0ba9b8cb35b5e04269c
veta306 <vv097099@gmail.com>
2024-01-23 17:54:32 +0200

    feat: complete 04-copy-directory using async/await

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,17 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const originalPath = path.join(__dirname, 'files');
 const copyPath = path.join(__dirname, 'files-copy');
 
-fs.rm(copyPath, { recursive: true, force: true }, (error) => {
-  if (error) return console.log(error.message);
-  fs.mkdir(copyPath, (error) => {
-    if (error) return console.log(error.message);
-    fs.readdir(originalPath, (error, files) => {
-      if (error) return console.log(error.message);
-      files.forEach((filename) => {
-        fs.copyFile(
-          path.join(originalPath, filename),
-          path.join(copyPath, filename),
-          (err) => {
-            if (err) {
-              console.log('Error Found:', err);
-            } else console.log('Copied ' + filename);
-          },
-        );
-      });
-    });
-  });
-});
+async function copyDir() {
+  await fs.rm(copyPath, { recursive: true, force: true });
+  await fs.mkdir(copyPath);
+  const files = await fs.readdir(originalPath);
+  for (const filename of files) {
+    await fs.copyFile(
+      path.join(originalPath, filename),
+      path.join(copyPath, filename),
+    );
+  }
+}
+copyDir();
